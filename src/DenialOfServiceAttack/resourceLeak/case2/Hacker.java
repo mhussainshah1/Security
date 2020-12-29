4bf39b77bce3602751a442581699bbc826dfedea
Muhammad Shah <muhammadshah.14@sunymaritime.edu>
2020-12-29 11:59:08 -0500

    initial commit

---
+++ src/DenialOfServiceAttack/resourceLeak/case2/Hacker.java
@@ -0,0 +1,13 @@
+package DenialOfServiceAttack.resourceLeak.case2;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class Hacker {
+    public static void main(String[] args) throws IOException {
+        var countLines = new CountLines();
+        while (true){
+            countLines.countLines(Path.of("src/DenialOfServiceAttack/resourceLeak/Hacker.txt"));
+        }
+    }
+}
