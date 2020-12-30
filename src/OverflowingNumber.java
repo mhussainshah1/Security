af021276139e6d7e57217759ba32acd80a8b6b26
Muhammad Shah <muhammadshah.14@sunymaritime.edu>
2020-12-30 09:09:11 -0500

    doPrivilledge()

---
+++ src/OverflowingNumber.java
@@ -0,0 +1,14 @@
+public class OverflowingNumber {
+    public static void main(String[] args) {
+        System.out.println(enoughRoomToAddLine(100));
+        System.out.println(enoughRoomToAddLine(2_000_000));
+        System.out.println(enoughRoomToAddLine(Integer.MAX_VALUE));
+    }
+
+    public static boolean enoughRoomToAddLine(int requestedSize) {
+        int maxLength = 1_000_000;
+        String newLine = "END OF FILE";
+        int newLineSize = newLine.length();
+        return requestedSize + newLineSize < maxLength;
+    }
+}
